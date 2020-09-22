8e6f9d6f2da450925384d80e80ae588a9186e6bb
myk245 <myk245@nyu.edu>
2020-09-22 16:35:34 -0400

    functions with keyword parameters

--- python_sandbox.py
+++ python_sandbox.py
@@ -49,3 +49,13 @@ def mult_x_add_y(number, x, y):
 mult_x_add_y(5, 2, 3)
 
 mult_x_add_y(1, 3, 1)
+
+
+# Define create_spreadsheet():
+def create_spreadsheet(title, row_count=1000):
+  print("Creating a spreadsheet called "+title +
+        " with " + str(row_count) + " rows")
+
+
+# Call create_spreadsheet() below with the required arguments:
+create_spreadsheet(title="Applications", row_count=10)
