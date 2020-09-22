b1fbefbb6a545952eb95d1644b79979c9df75ee3
myk245 <myk245@nyu.edu>
2020-09-22 16:28:03 -0400

    functions with parameters

--- python_sandbox.py
+++ python_sandbox.py
@@ -41,3 +41,11 @@ print(customer_one_itemization)
 print('Customer One Total:')
 
 print(customer_one_total)
+
+# functions with parameters
+def mult_x_add_y(number, x, y):
+  print(number*x + y)
+
+mult_x_add_y(5, 2, 3)
+
+mult_x_add_y(1, 3, 1)
