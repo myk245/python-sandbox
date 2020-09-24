78e3ae1cc4df2982cde1a59f97ef474cbc242fc8
myk245 <myk245@nyu.edu>
2020-09-24 17:40:42 -0400

    function to convert f to c

--- python_sandbox.py
+++ python_sandbox.py
@@ -73,3 +73,13 @@ low, high = get_boundaries(100, 20)
 
 print(low)
 print(high)
+
+
+def f_to_c(f_temp):
+  c_temp = ((f_temp) - 32) * 5/9
+  return c_temp
+
+
+f100_in_celsius = f_to_c(100)
+
+print(f100_in_celsius)
