02730cfa72125d005ac70154385010d2c917a2f1
myk245 <myk245@nyu.edu>
2020-09-25 15:50:33 -0400

    energy function

--- python_sandbox.py
+++ python_sandbox.py
@@ -102,3 +102,16 @@ def get_force(mass, acceleration):
 train_force = get_force(train_mass, train_acceleration)
 
 print(train_force)
+
+print('The GE train supplies ' + str(train_force) + ' Newtons of force.')
+
+
+def get_energy(mass, c=3*10**8):
+  return mass*c**2
+
+
+bomb_energy = get_energy(bomb_mass)
+
+print(bomb_energy)
+
+print('A 1kg bomb supplies ' + str(bomb_energy) + ' Joules.')
