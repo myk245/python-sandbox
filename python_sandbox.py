deb84236bb315d8946168b0656350e61fafd0696
myk245 <myk245@nyu.edu>
2020-09-25 15:45:50 -0400

    function for force

--- python_sandbox.py
+++ python_sandbox.py
@@ -93,3 +93,12 @@ def c_to_f(c_temp):
 c0_in_fahrenheit = c_to_f(0)
 
 print(c0_in_fahrenheit)
+
+
+def get_force(mass, acceleration):
+  return mass * acceleration
+
+
+train_force = get_force(train_mass, train_acceleration)
+
+print(train_force)
