1ae6cc77fba146b8a7b7e66c9fc3824e3e992cc3
myk245 <myk245@nyu.edu>
2020-09-25 15:43:37 -0400

    c to f function

--- python_sandbox.py
+++ python_sandbox.py
@@ -83,3 +83,13 @@ def f_to_c(f_temp):
 f100_in_celsius = f_to_c(100)
 
 print(f100_in_celsius)
+
+
+def c_to_f(c_temp):
+  f_temp = ((c_temp) * (9/5)) + 32
+  return f_temp
+
+
+c0_in_fahrenheit = c_to_f(0)
+
+print(c0_in_fahrenheit)
