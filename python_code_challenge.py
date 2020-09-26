05a212ea97d15dd0ed2d5e2a4182a54bf6e284cc
myk245 <myk245@nyu.edu>
2020-09-26 19:43:59 -0400

    square root function

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -10,3 +10,15 @@ print(tenth_power(0))
 # 0 to the 10th power is 0
 print(tenth_power(2))
 # 2 to the 10th power is 1024
+
+
+# Write your square_root function here:
+def square_root(num):
+  return num ** 0.5
+
+
+# Uncomment these function calls to test your square_root function:
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
