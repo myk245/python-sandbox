e5e9c7ff473e916b40631d197569074d3f0c5fb5
myk245 <myk245@nyu.edu>
2020-09-26 22:49:58 -0400

    average function

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -2,7 +2,6 @@
 def tenth_power(num):
   return num ** 10
 
-
 # Uncomment these function calls to test your tenth_power function:
 print(tenth_power(1))
 # 1 to the 10th power is 1
@@ -16,22 +15,31 @@ print(tenth_power(2))
 def square_root(num):
   return num ** 0.5
 
-
 # Uncomment these function calls to test your square_root function:
 print(square_root(16))
 # should print 4
 print(square_root(100))
 # should print 10
 
+
 # Write your win_percentage function here:
-
-
 def win_percentage(wins, losses):
    return wins/(wins + losses) * 100
-
 
 # Uncomment these function calls to test your win_percentage function:
 print(win_percentage(5, 5))
 # should print 50
 print(win_percentage(10, 0))
 # should print 100
+
+
+# Write your average function here:
+def average(num1, num2):
+  return (num1 + num2) / 2
+
+# Uncomment these function calls to test your average function:
+print(average(1, 100))
+# The average of 1 and 100 is 50.5
+print(average(1, -1))
+# The average of 1 and -1 is 0
+
