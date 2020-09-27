c7cdfe6166a745f16b2b2c028ed08917e6a13deb
myk245 <myk245@nyu.edu>
2020-09-27 18:27:10 -0400

    finishes up function code challenge

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -96,3 +96,20 @@ print(dog_years("Lola", 16))
 # should print "Lola, you are 112 years old in dog years"
 print(dog_years("Baby", 0))
 # should print "Baby, you are 0 years old in dog years"
+
+
+# Write your lots_of_math function here:
+def lots_of_math(a, b, c, d):
+  first = a + b
+  second = c - d
+  third = first * second
+  fourth = third % a
+  print(first)
+  print(second)
+  print(third)
+  return fourth
+# Uncomment these function calls to test your lots_of_math function:
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
