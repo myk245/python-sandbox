7627875dca7d12b04ff57494fc605be431765919
myk245 <myk245@nyu.edu>
2020-09-27 18:22:25 -0400

    dog years function

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -85,3 +85,14 @@ print(introduction("James", "Bond"))
 # should print Bond, James Bond
 print(introduction("Maya", "Angelou"))
 # should print Angelou, Maya Angelou
+
+
+# Write your dog_years function here:
+def dog_years(name, age):
+  dog_age = age * 7
+  return name + ', you are ' + str(dog_age) + ' years old in dog years'
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
