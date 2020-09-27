a54fe649072cc3d1f76e3f77d6913c3749ab91e1
myk245 <myk245@nyu.edu>
2020-09-26 22:53:05 -0400

    remainder function

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -43,3 +43,13 @@ print(average(1, 100))
 print(average(1, -1))
 # The average of 1 and -1 is 0
 
+
+# Write your remainder function here:
+def remainder(num1, num2):
+  return (num1 * 2) % (num2 / 2)
+
+# Uncomment these function calls to test your remainder function:
+print(remainder(15, 14))
+# should print 2
+print(remainder(9, 6))
+# should print 0
