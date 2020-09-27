f022375e172f1199780307c87d2262bcac4c2105
myk245 <myk245@nyu.edu>
2020-09-27 18:17:58 -0400

    tip function

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -65,3 +65,13 @@ first_three_multiples(10)
 # should print 10, 20, 30, and return 30
 first_three_multiples(0)
 # should print 0, 0, 0, and return 0
+
+
+# Write your tip function here:
+def tip(total, percentage):
+  return total * (percentage / 100)
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
