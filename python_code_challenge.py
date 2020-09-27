a9ad2fe82551806f934601a1f9c79ea6adce3b20
myk245 <myk245@nyu.edu>
2020-09-26 22:46:12 -0400

    win percentages

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -22,3 +22,17 @@ print(square_root(16))
 # should print 4
 print(square_root(100))
 # should print 10
+
+# Write your win_percentage function here:
+
+
+def win_percentage(wins, losses):
+  percent_win = wins/(wins + losses) * 100
+  return percent_win
+
+
+# Uncomment these function calls to test your win_percentage function:
+print(win_percentage(5, 5))
+# should print 50
+print(win_percentage(10, 0))
+# should print 100
