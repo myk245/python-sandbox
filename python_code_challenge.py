a7cc7ec4401a71c8422abe5110e3604c41e0a6f0
myk245 <myk245@nyu.edu>
2020-09-29 17:13:38 -0400

    additional boolean operators

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -134,3 +134,13 @@ def graduation_reqs(gpa, credits):
 def graduation_mailer(gpa, credits):
   if (credits >= 120) or (gpa >= 2.0):
     return True
+
+def graduation_reqs(gpa, credits):
+  if (gpa >= 2.0) and (credits >= 120):
+    return "You meet the requirements to graduate!"
+  if (gpa >= 2.0) and (credits < 120):
+    return "You do not have enough credits to graduate."
+  if (gpa < 2.0) and (credits >= 120):
+    return "Your GPA is not high enough to graduate."
+  if (gpa < 2.0) and (credits < 120):
+    return "You do not meet either requirement to graduate!"
