24fd10a3cd6e893a3e97acaec40d9a93bce1bb77
myk245 <myk245@nyu.edu>
2020-09-29 22:12:47 -0400

    control flow

--- python_code_challenge.py
+++ python_code_challenge.py
@@ -180,3 +180,12 @@ try:
   raises_value_error()
 except ValueError:
   print("You raised a ValueError!")
+
+# Control Flow
+def applicant_selector(gpa, ps_score, ec_count):
+  if (gpa >= 3.0) and (ps_score >= 90) and (ec_count >= 3):
+    return "This applicant should be accepted."
+  elif (gpa >= 3.0) and (ps_score >= 90) and (ec_count < 3):
+    return "This applicant should be given an in-person interview."
+  else:
+    return "This applicant should be rejected."
